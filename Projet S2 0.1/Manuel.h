ed7505ca992ea6e11b742af2234401614b66593c
PierreC12033 <pierre.camugli@edu.ece.fr>
2018-04-04 00:18:48 +0200

    ajout des différents fichiers des menus

---
+++ Projet S2 0.1/Manuel.h
@@ -0,0 +1,7 @@
+#ifndef MANUEL_H
+#define MANUEL_H
+
+///rtfm
+void Manuel();
+
+#endif // MANUEL_H
---
+++ Projet S2 0.1/Menu1.h
@@ -0,0 +1,8 @@
+#ifndef MENU1_H
+#define MENU1_H
+#include <iostream>
+/// simulation ou annalyse
+void Menu1(std::string name_ecosys);
+
+
+#endif // MENU1_H

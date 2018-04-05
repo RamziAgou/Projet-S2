bfa0f8980f848940b58cb89167088e75c63b32ee
alexdissy <alexandre.avakian@edu.ece.fr>
2018-04-05 10:58:43 +0200

    Ajout btn
    charger - edition

--- Projet S2 0.2/GrapheInterface.cpp
+++ Projet S2 0.2/GrapheInterface.cpp
@@ -4,7 +4,7 @@
 /// éléments qui seront ensuite ajoutés lors de la mise ne place du Graphe
 GraphInterface::GraphInterface(int x, int y, int w, int h)
 {
-    m_top_box.set_dim(1000,740);
+     m_top_box.set_dim(1000,740);
     m_top_box.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Up);
 
     m_top_box.add_child(m_tool_box);
@@ -13,12 +13,11 @@ GraphInterface::GraphInterface(int x, int y, int w, int h)
     m_tool_box.set_bg_color(BLANCBLEU);
 
 
+    //bouton sauvegarder
     m_tool_box.add_child( m_bouton_sauver );
     m_bouton_sauver.set_frame(3,3,77,60);
-    //m_boite_boutons.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Up );
     m_bouton_sauver.set_bg_color(BLANC);
 
-    //bouton sauvegarder
     m_bouton_sauver.add_child(m_bouton_sauver_label1);
     m_bouton_sauver_label1.set_frame(14,25,100,100);
     m_bouton_sauver_label1.set_message("Sauve-");
@@ -26,9 +25,18 @@ GraphInterface::GraphInterface(int x, int y, int w, int h)
     m_bouton_sauver_label2.set_frame(14,35,100,100);
     m_bouton_sauver_label2.set_message("garder");
 
+    //bouton charger
+    m_tool_box.add_child( m_bouton_charger );
+    m_bouton_charger.set_frame(3,63,77,60);
+    m_bouton_charger.set_bg_color(BLANC);
+    m_bouton_charger.add_child(m_bouton_charger_label);
+    m_bouton_charger_label.set_message("Charger");
+
+
+
     //bouton quitter
     m_tool_box.add_child( m_bouton_quitter );
-    m_bouton_quitter.set_frame(3,63,77,60);
+    m_bouton_quitter.set_frame(3,123,77,60);
     m_bouton_quitter.set_bg_color(BLANC);
     m_bouton_quitter.add_child(m_bouton_quitter_label);
     m_bouton_quitter_label.set_message("Quitter");
@@ -40,7 +48,7 @@ GraphInterface::GraphInterface(int x, int y, int w, int h)
     m_bouton_pause.add_child(m_bouton_pause_image);
     m_bouton_pause_image.set_pic_name("bouton_pause.jpg");
 
-    m_tool_box.add_child( m_bouton_play );
+    //m_tool_box.add_child( m_bouton_play );
     m_bouton_play.set_dim(30,30);
     m_bouton_play.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Down);
     m_bouton_play.add_child(m_bouton_play_image);
@@ -50,15 +58,21 @@ GraphInterface::GraphInterface(int x, int y, int w, int h)
     m_bouton_play_pause_label.set_frame(5,675,100,100);
     m_bouton_play_pause_label.set_message("Simulation");
 
+    m_tool_box.add_child( m_edition_label );
+    m_edition_label.set_frame(5,210,100,100);
+    m_edition_label.set_message("EDITER :");
 
-    //m_bouton_play_pause_label.set_message("Sauve-");
+    m_tool_box.add_child( m_sommet_label );
+    m_sommet_label.set_frame(5,230,100,100);
+    m_sommet_label.set_message("Sommet");
+
+
 
 
     m_top_box.add_child(m_main_box);
     m_main_box.set_dim(908,720);
     m_main_box.set_gravity_xy(grman::GravityX::Right, grman::GravityY::Up);
     m_main_box.set_bg_color(BLANCJAUNE);
-
 
 }
 
--- Projet S2 0.2/GrapheInterface.h
+++ Projet S2 0.2/GrapheInterface.h
@@ -21,24 +21,30 @@ class GraphInterface
         /// Dans cette boite seront ajoutés des boutons de contrôle etc...
         grman::WidgetBox m_tool_box;
 
- //       grman::WidgetBox m_boite_boutons;    // Sera la boite à boutons en haut à droite
-        grman::WidgetButton m_bouton_sauver;       // Sera le bouton avec le texte NEW
+
+        grman::WidgetButton m_bouton_sauver;
         grman::WidgetButton m_bouton_quitter;
+        grman::WidgetButton m_bouton_charger;
 
         grman::WidgetText m_bouton_sauver_label1;
         grman::WidgetText m_bouton_sauver_label2;
         grman::WidgetText m_bouton_quitter_label;
+        grman::WidgetText m_sommet_label;
 
+        grman::WidgetText m_bouton_charger_label;
 
-        grman::WidgetButton m_bouton_pause;       // Sera le bouton avec l'image de clown méchant
-        grman::WidgetImage m_bouton_pause_image;  // L'image de clown méchant
+        grman::WidgetButton m_bouton_pause;
+        grman::WidgetImage m_bouton_pause_image;
 
-        grman::WidgetButton m_bouton_play;       // Sera le bouton avec l'image de clown méchant
+        grman::WidgetButton m_bouton_play;
         grman::WidgetImage m_bouton_play_image;
 
         grman::WidgetText m_bouton_play_pause_label;
+
+        grman::WidgetText m_edition_label;
         // A compléter éventuellement par des widgets de décoration ou
         // d'édition (boutons ajouter/enlever ...)
+
 
     public :
 
